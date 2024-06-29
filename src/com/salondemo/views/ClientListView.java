52c6ad1995bff9b6018de5ab0e87d55629fd7b12
brunobahri <brunobahri27@gmail.com>
2024-06-29 20:36:45 -0300

    feat: create ClientListView.java

---
+++ src/com/salondemo/views/ClientListView.java
@@ -0,0 +1,26 @@
+package com.salondemo.views;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+import com.salondemo.models.Client;
+
+public class ClientListView extends JPanel {
+    private JList<String> clientJList;
+    private DefaultListModel<String> listModel;
+
+    public ClientListView() {
+        setLayout(new BorderLayout());
+
+        listModel = new DefaultListModel<>();
+        clientJList = new JList<>(listModel);
+        add(new JScrollPane(clientJList), BorderLayout.CENTER);
+    }
+
+    public void updateClientList(List<Client> clients) {
+        listModel.clear();
+        for (Client client : clients) {
+            listModel.addElement(client.getName() + " - " + client.getPhoneNumber());
+        }
+    }
+}

cabd98ac0c31a110c13c38a2d41c5e1a728ba923
brunobahri <brunobahri27@gmail.com>
2024-06-29 20:35:42 -0300

    feat: controller for ClientList

--- src/com/salondemo/controllers/ClientController.java
+++ src/com/salondemo/controllers/ClientController.java
@@ -1,7 +1,9 @@
 package com.salondemo.controllers;
 
 import com.salondemo.models.Client;
+import com.salondemo.models.ClientList;
 import com.salondemo.views.ClientView;
+import com.salondemo.views.ClientListView;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -9,18 +11,24 @@ import java.awt.event.ActionListener;
 
 public class ClientController {
     private Client model;
-    private ClientView view;
+    private ClientView clientView;
+    private ClientListView clientListView;
+    private ClientList clientList;
 
-    public ClientController(Client model, ClientView view) {
+    public ClientController(Client model, ClientView clientView, ClientListView clientListView, ClientList clientList) {
         this.model = model;
-        this.view = view;
+        this.clientView = clientView;
+        this.clientListView = clientListView;
+        this.clientList = clientList;
 
         // Adiciona listener ao botão de salvar
-        this.view.getSaveButton().addActionListener(new ActionListener() {
+        this.clientView.getSaveButton().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                model.setName(view.getName());
-                model.setPhoneNumber(view.getPhoneNumber());
+                model.setName(clientView.getName());
+                model.setPhoneNumber(clientView.getPhoneNumber());
+                clientList.addClient(new Client(model.getName(), model.getPhoneNumber()));
+                clientListView.updateClientList(clientList.getClients());
                 JOptionPane.showMessageDialog(null, "Cliente salvo com sucesso!");
             }
         });
